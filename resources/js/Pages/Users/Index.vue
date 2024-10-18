e497ed5cb6efebdd31a92b75491a217d12c9f82e
vldsy <vldsyata@gmail.com>
2024-10-18 14:40:48 +0200

    users crud: index

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,22 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { useForm, usePage, Head } from '@inertiajs/vue3';
+
+const props = defineProps(['users']);
+
+</script>
+
+<template>
+
+    <Head title="Authors" />
+
+    <AuthenticatedLayout>
+
+        <pre>{{ JSON.stringify($page.props.users, null, 2) }}</pre>
+
+        <div class="overflow-x-auto">
+            
+        </div>
+
+    </AuthenticatedLayout>
+</template>
