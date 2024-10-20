325a84e8acde81f9dbddc17044e8c4af611446ce
XoDeR <vldsyata@gmail.com>
2024-10-20 17:41:43 +0200

    copied views from client

---
+++ resources/js/Pages/Clients/Create.vue
@@ -0,0 +1,112 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import Checkbox from '@/Components/Checkbox.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    first_name: '',
+    last_name: '',
+    email: '',
+    address: '',
+    phone_number: '',
+    password: '',
+    terms_accepted: true,
+});
+
+const submit = () => {
+    form.post(route('users.store'));
+};
+</script>
+
+<template>
+
+    <Head title="Create Client" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Create Client
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                <div class="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                    <form @submit.prevent="submit">
+                        <div>
+                            <InputLabel for="first_name" value="First Name" />
+
+                            <TextInput id="first_name" type="text" class="mt-1 block w-full" v-model="form.first_name"
+                                required autofocus autocomplete="given-name" />
+
+                            <InputError class="mt-2" :message="form.errors.first_name" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="last_name" value="Last Name" />
+
+                            <TextInput id="last_name" type="text" class="mt-1 block w-full" v-model="form.last_name"
+                                required autofocus autocomplete="family-name" />
+
+                            <InputError class="mt-2" :message="form.errors.last_name" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="email" value="Email" />
+
+                            <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required
+                                autocomplete="email" />
+
+                            <InputError class="mt-2" :message="form.errors.email" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="address" value="Address" />
+
+                            <TextInput id="address" type="text" class="mt-1 block w-full" v-model="form.address"
+                                required autofocus autocomplete="address" />
+
+                            <InputError class="mt-2" :message="form.errors.address" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="phone_number" value="Phone Number" />
+
+                            <TextInput id="phone_number" type="text" class="mt-1 block w-full"
+                                v-model="form.phone_number" required autofocus autocomplete="phone number" />
+
+                            <InputError class="mt-2" :message="form.errors.phone_number" />
+                        </div>
+
+                        <div class="mt-4">
+                            <InputLabel for="password" value="Password" />
+
+                            <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password"
+                                required autocomplete="new-password" />
+
+                            <InputError class="mt-2" :message="form.errors.password" />
+                        </div>
+
+                        <div class="mt-4 block">
+                            <label class="flex items-center">
+                                <Checkbox name="terms_accepted" v-model:checked="form.terms_accepted" />
+                                <span class="ms-2 text-sm text-gray-600">Accept terms</span>
+                            </label>
+                        </div>
+
+                        <div class="mt-4 flex items-center justify-start">
+                            <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }"
+                                :disabled="form.processing">
+                                Save
+                            </PrimaryButton>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/Clients/Index.vue
@@ -0,0 +1,95 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { useForm, usePage, Head } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
+import DataTable from '@/Components/DataTable.vue'
+import { ref, computed, onMounted } from 'vue'
+import Pagination from '@/Components/Pagination.vue'
+
+const props = defineProps(['users']);
+
+// const columns = computed(() => {
+//   return [
+//     {
+//       key: 'first_name',
+//       label: 'First Name'
+//     },
+//     {
+//       key: 'last_name',
+//       label: 'Last Name'
+//     },
+//     {
+//       key: 'email',
+//       label: 'Email'
+//     },
+//   ]
+
+// })
+
+// const rows = computed(() => {
+//   return props.users.map(({ id, first_name, last_name }) => ({ id, first_name, last_name }));
+// })
+
+const fields = computed(() => {
+    return [
+        { key: 'first_name', label: 'First Name' },
+        { key: 'last_name', label: 'Last Name' },
+        { key: 'email', label: 'Email' },
+        { key: 'actions', label: 'Actions' }
+    ]
+});
+
+const rows = computed(() => {
+    return props.users.data.map(({ id, first_name, last_name, email }) => ({ id, first_name, last_name, email }));
+});
+
+function confirmDelete(e) {
+    if (!window.confirm(" Are you sure？")) {
+        e.preventDefault();
+    }
+}
+
+</script>
+
+<template>
+
+    <Head title="Users" />
+
+    <AuthenticatedLayout>
+        <!-- <pre>{{ JSON.stringify($page.props.users, null, 2) }}</pre> -->
+        <pre>{{ JSON.stringify($page.props.auth.can, null, 2) }}</pre>
+
+        <div class="overflow-x-auto">
+            <Link :href="route('users.create')" v-if="$page.props.auth.can['manage users']"
+                class="ml-6 rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            Add new user
+            </Link>
+
+            <DataTable class="mt-4" :rows="rows" :fields="fields">
+                <template #header="{ label }">
+                    <span v-text="label" />
+                </template>
+                <template #cell(actions)="{ item }">
+                    <!-- <code v-text="JSON.stringify(item, null, 2)" :style="{
+            whiteSpace: 'pre-wrap',
+            textAlign: 'left',
+            display: 'block',
+            paddingLeft: '3rem'
+          }" /> -->
+                    <div class="flex items-center px-0 py-3">
+                        <Link as="button" :href="route('users.edit', item.id)" method="get"
+                            class="mr-2 flex items-center justify-center rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                        Edit
+                        </Link>
+                        <Link as="button" :href="route('users.destroy', item.id)" method="delete"
+                            @click="(e) => confirmDelete(e)"
+                            class="flex items-center justify-center rounded-lg bg-red-700 px-4 py-2 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                        Delete
+                        </Link>
+                    </div>
+                </template>
+            </DataTable>
+            <pagination :links="users.links" />
+        </div>
+    </AuthenticatedLayout>
+</template>
