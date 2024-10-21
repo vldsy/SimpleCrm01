0afba8bbef82e213b4ebfabc669e6883b61cbd97
XoDeR <vldsyata@gmail.com>
2024-10-21 18:57:21 +0200

    task model and task status

--- database/migrations/2024_10_21_164706_create_tasks_table.php
+++ database/migrations/2024_10_21_164706_create_tasks_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->foreingId('user_id')->constrained();
+            $table->foreingId('client_id')->constrained();
+            $table->foreingId('project_id')->constrained();
+            $table->date('deadline_at');
+            $table->string('status');
             $table->timestamps();
         });
     }
